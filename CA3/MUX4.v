c465761daadc0651b2d6b99e00487ce34de117c3
rmeta <91409411+rmeta@users.noreply.github.com>
2024-08-01 21:49:55 -0700

    Delete CA3 directory

--- CA3/MUX4.v
+++
@@ -1,8 +0,0 @@
-module MUX4(W_out, A, B, C, D, S);
-	parameter N = 8;
-	output [N-1:0]W_out;
-	input [N-1:0]A, B, C, D;
-	input [1:0]S;
-	
-	assign W_out = (S==2'b00) ? A: (S==2'b01) ? B: (S==2'b10) ? C: D;
-endmodule
